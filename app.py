75277ae1e1939a96cda084ce7edf32a7bb9ec664
Edcarloszaya <edcarlos.molequedoido@gmail.com>
2024-10-09 10:34:30 -0300

    configura a secrete_key. importa a rota auth_pb de auth_controller e registra essa rota

--- app.py
+++ app.py
@@ -2,12 +2,17 @@
 
 from flask import Flask
 from personal_blog.controller.post_controller import post_bp
+from personal_blog.controller.auth_controller import auth_bp
 
 
 app = Flask(__name__)
 
+# configura secret_key 
+app.config['SECRET_KEY'] = 'asugsuifghsaipghaispghipasghipasghipasghpa'
+
 # Registra as rotas
 app.register_blueprint(post_bp)
+app.register_blueprint(auth_bp)
 
 
 if __name__ == '__main__':
