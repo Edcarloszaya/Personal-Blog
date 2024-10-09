ff637dc1d36044d0a5547a8cd0726a4544be0df7
Edcarloszaya <edcarlos.molequedoido@gmail.com>
2024-10-09 10:54:26 -0300

    feat: Implementa as rotas do blog
    - Define o blueprint `post_bp` para o controlador rotas dom post, configurando o diretório de templates.
    - Cria rota `/home` para mostra todos os posts.
    - Cria rota `/post/<int:post_id` para mostra o conteudo de post.
    - Cria rota `/admin`para manipulacao dos posts precisa de user autenticado e sendo admin.
    - Cria rota `/edit` com formulário para edicao do post , precisa ser admin pra ter acesso a essa rota.
     Cria rota `/new` com formulário para cria um novo   post , precisa ser admin pra ter acesso a essa rota.
     Cria rota `/delete` para deletar um post precisa ser admin pra ter acesso a rota.
    
     as rotas com @requires_admin precisa ter acesso admin

--- personal_blog/controller/post_controller.py
+++ personal_blog/controller/post_controller.py
@@ -1,18 +1,84 @@
-# app/controllers/post_controller.py
 
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from personal_blog.models.post import Post
+from personal_blog.models.forms import EditPost, NewPost
+from personal_blog.controller.auth_controller import requires_admin
+
+# onde a  post_controller busca os templates
+post_bp = Blueprint("post", __name__, template_folder="../views/templates")
 
 
-post_bp = Blueprint('post', __name__,template_folder='../views/templates')
-
-@post_bp.route('/')
+@post_bp.route("/")
 def home():
-    posts = Post.get_all() # Exemplo de função que busca os posts
-    
-    return render_template('home.html',posts=posts)
+    # home lista todos os posts
+    posts = Post.get_all()
 
-@post_bp.route('/post/<int:post_id>')
+    return render_template("home.html", posts=posts)
+
+
+@post_bp.route("/post/<int:post_id>")
 def post_detail(post_id):
-    post = Post.get_by_id(post_id)  # Função que busca o post específico
-    return render_template('post_content.html',post=post)
+    # mostra um post especifico pelo id
+    post = Post.get_by_id(post_id)
+
+    return render_template("post_content.html", post=post)
+
+
+@post_bp.route("/admin")
+@requires_admin
+def admin():
+    # requer admin logado pra acessa os post e pra delete edit e add novos post
+    posts = Post.get_all()
+
+    return render_template("admin.html", posts=posts)
+
+
+@post_bp.route("/edit/<int:post_id>", methods=["GET", "POST"])
+@requires_admin
+def edit(post_id):
+    # editar um post especifico pelo id e exibir um feedback
+    post = Post.get_by_id(post_id)
+
+    form = EditPost()
+
+    if request.method == "POST" and form.validate_on_submit():
+        post["title"] = form.title.data
+        post["date"] = form.published_date.data
+        post["content"] = form.content.data
+
+        Post.edit_post(post, post_id)
+
+        flash("post atualizado com sucesso")
+        return redirect(url_for("post.admin"))
+
+    return render_template("edit.html", form=form, post=post)
+
+
+@post_bp.route("/new", methods=["GET", "POST"])
+@requires_admin
+def new_post():
+    # adiciona um novo post e exibir um feedback
+    post = {}
+
+    form = NewPost()
+
+    if request.method == "POST" and form.validate_on_submit():
+        post["title"] = form.title.data
+        post["date"] = form.published_date.data
+        post["content"] = form.content.data
+        Post.create(post)
+
+        flash("post criado com sucesso")
+        return redirect(url_for("post.admin"))
+
+    return render_template("new.html", form=form)
+
+
+@post_bp.route("/delete/<int:post_id>", methods=["GET"])
+@requires_admin
+def delete(post_id):
+    # deletar um post e exibir um feedback
+    Post.delete(post_id)
+    flash("post deletado com sucesso")
+
+    return redirect(url_for("post.admin"))
